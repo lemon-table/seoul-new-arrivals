fc4dda6cdc18bd8fd0543249af58cb40a38b0e7c
=lemon-table <=>
2024-05-10 10:16:40 +0900

    fix: app.js 정적파일 경로 수정

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import cors from "cors";
 import router from "./src/routes/index.js";
 import ErrorHandlingMiddleware from "./src/middlewares/error-handling.middleware.js"
 import sequelize from './src/models/index.js'; 
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 //.env에 있는 여러 값들을, prosess.env 객체 안에 추가하게 된다.
 dotEnv.config();
@@ -17,7 +19,15 @@ app.use(express.json());
 app.use(cookieParser());
 //app.use(express.static("src/assets"));
 // 정적 파일의 루트 경로를 '/'로 설정
-app.use("/", express.static("src/assets"));
+//app.use("/", express.static("src/assets"));
+//app.use(express.static(path.join(__dirname, 'src/assets')));
+
+// 현재 파일의 URL을 파일 경로로 변환
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// 정적 파일 경로 설정
+app.use(express.static(path.join(__dirname, 'src/assets')));
 
 app.use("/api", router);
 app.use(ErrorHandlingMiddleware);
